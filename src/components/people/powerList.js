f746b24cd20f347812a20c53a2757cf115d73631
janpauldahlke <janpauldahlke@gmail.com>
2018-09-03 15:42:38 +0200

    improves power Letter list display logic

--- src/components/people/powerList.js
+++ src/components/people/powerList.js
@@ -4,22 +4,52 @@ import {withRouter} from "react-router-dom";
 
 class PowerList extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+
+    this.state = {
+      letter: props.match.params.letter
+    }
   }
 
+  // not this nice and a little durch die brust durchs auge, aber immerin läuft
+  componentWillReceiveProps = (nextProps) => {
+    this.setState({
+      letter: nextProps.match.params.letter
+    }) 
+  }
+
+  //functions helpers
   powerListHelper(p) {
-    return p.map((power) => {
+    return p.map((power, index) => {
 
       return (
-        <p>{power}</p>
+        <p key={index}>{power}</p>
       );
     })
   }
 
+  getPowers =  () => {
+    const letter = this.state.letter;
+    const powers = this.props.powers;
+
+    let knownPowers = [];
+
+    for(let i= 0;i<powers.length;i++){
+      if(powers[i].toUpperCase().startsWith(letter)){
+        knownPowers.push(powers[i])
+      }
+    }
+    return knownPowers
+  }
+
+  //---------renderers
+
   render() {
+    
+    let knownPowers = this.getPowers();
     return (
       <div>
-       {this.powerListHelper(this.props.powers)}
+       {this.powerListHelper(knownPowers)}
       </div>
     );
 
